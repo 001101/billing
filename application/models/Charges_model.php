2e81dfada64250efd782e3b5b7ab339013c3b7a3
John Kenneth Tolentino <tolentinojohnkenneth007@gmail.com>
2017-02-24 13:33:15 +0800

    Bug Fixed on Charges model
    missing letter 's' on charges...

--- application/models/Charges_model.php
+++ application/models/Charges_model.php
@@ -14,7 +14,7 @@
         	$sql="INSERT INTO `charges` (`charge_id`, `charge_name`, `charge_description`, `charge_amount`) VALUES
         		(1,'Retainer\'s Fee', 'Monthly Retainer\'s Fee',0),
         		(2,'Previous Balance', 'Previous Balance',0)
-            ON DUPLICATE KEY UPDATE charges.charge_name=VALUES(charges.charge_name),charges.charge_description=VALUES(charge.charge_description)";
+            ON DUPLICATE KEY UPDATE charges.charge_name=VALUES(charges.charge_name),charges.charge_description=VALUES(charges.charge_description)";
 
         $this->db->query($sql);
     }
